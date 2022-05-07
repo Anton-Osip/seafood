32846f15af4273aa4f2332aa935abddf8272f061
Anton-Osip <63441692+Anton-Osip@users.noreply.github.com>
2022-05-08 00:55:06 +0300

    header__content

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-$(function(){
-
-});
+$(function () {
+	$('.header-slider').slick({
+		dots: true,
+		vertical: true,
+		prevArrow: `<button rype='button' class='slick-prev'><img src="./images/prev.svg" alt=""> </button>`,
+		nextArrow: `<button rype="button" class="slick-next"><img src="./images/next.svg" alt=""></button>`,
+	})
+})
