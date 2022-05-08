8bcb9ebfa60b0ff6777da24b5f28589f9de96648
Anton-Osip <63441692+Anton-Osip@users.noreply.github.com>
2022-05-08 12:21:17 +0300

    seafood

--- js/main.js
+++ js/main.js
@@ -5,4 +5,21 @@ $(function () {
 		prevArrow: `<button rype='button' class='slick-prev'><img src="./images/prev.svg" alt=""> </button>`,
 		nextArrow: `<button rype="button" class="slick-next"><img src="./images/next.svg" alt=""></button>`,
 	})
+
+	$('.product__name').slick({
+		slidesToShow: 6,
+		slidesToScroll: 1,
+		focusOnSelect: true,
+		asNavFor: '.product__content',
+		vertical: true,
+		prevArrow: `<button rype='button' class='product-prev'><img src="./images/product-prev.png" alt=""> </button>`,
+		nextArrow: `<button rype="button" class="product-next"><img src="./images/product-next.png" alt=""></button>`,
+	})
+	$('.product__content').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		asNavFor: '.product__name',
+		fade: true,
+		arrows: false,
+	})
 })
