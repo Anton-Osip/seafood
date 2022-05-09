9265ef9c3e82319d085ee9f23e6a25d9741e90d8
Anton-Osip <63441692+Anton-Osip@users.noreply.github.com>
2022-05-09 11:05:58 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -4,6 +4,18 @@ $(function () {
 		vertical: true,
 		prevArrow: `<button rype='button' class='slick-prev'><img src="./images/prev.svg" alt=""> </button>`,
 		nextArrow: `<button rype="button" class="slick-next"><img src="./images/next.svg" alt=""></button>`,
+		responsive: [
+			{
+				breakpoint: 371,
+				settings: {
+					dots: false,
+					arrows: false,
+					autoplay: true,
+					autoplaySpeed: 2000,
+					vertical: false,
+				},
+			},
+		],
 	})
 
 	$('.product__name').slick({
@@ -14,6 +26,26 @@ $(function () {
 		vertical: true,
 		prevArrow: `<button rype='button' class='product-prev'><img src="./images/product-prev.png" alt=""> </button>`,
 		nextArrow: `<button rype="button" class="product-next"><img src="./images/product-next.png" alt=""></button>`,
+		responsive: [
+			{
+				breakpoint: 891,
+				settings: {
+					vertical: false,
+					slidesToShow: 3,
+					arrows: false,
+					dots: true,
+				},
+			},
+			{
+				breakpoint: 461,
+				settings: {
+					slidesToShow: 1,
+					arrows: false,
+					vertical: false,
+					dots: true,
+				},
+			},
+		],
 	})
 	$('.product__content').slick({
 		slidesToShow: 1,
@@ -21,5 +53,8 @@ $(function () {
 		asNavFor: '.product__name',
 		fade: true,
 		arrows: false,
+	})
+	$('.menu__btn').on('click', function () {
+		$('.menu__list').toggleClass('menu__list--active')
 	})
 })
